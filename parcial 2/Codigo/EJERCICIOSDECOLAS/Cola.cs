54af6889207ce1f6f02808371890b29957355c42
Adrian Ramirez Hernandez <ramirezhdzadrian@gmail.com>
2025-10-24 17:13:34 -0500

    Add files via upload

---
+++ parcial 2/Codigo/EJERCICIOSDECOLAS/Cola.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EJERCICIOSDECOLAS
+{
+    internal class Cola
+    {
+        private int Max = 0;
+        private char[] Arreglo;
+        private int frente;
+        private int final;
+        private int tamano;
+       
+        public Cola (int Max)
+        {
+            this.Max = Max;
+            Arreglo=new char[Max];
+            frente = 0;
+            final = -1;
+            tamano = 0;
+        }
+        public bool EstaVacia()
+        {
+            return tamano == 0;
+        }
+        public bool EstaLLena()
+        {
+            return tamano == Max;
+        }
+
+        public void Insertar(char valor)
+        {
+            if (EstaLLena())
+            {
+                Console.WriteLine("Ësta LLena");
+            }
+            final = (final + 1) % Max;
+            Arreglo[final] = valor;
+            tamano++;
+        }
+        public char Eliminar()
+        {
+            if (EstaVacia()) {
+                Console.WriteLine("Esta vacia");
+                return '0';
+            }
+            char valor = Arreglo[frente];
+            frente = (frente + 1) % Max;
+            tamano--;
+            return valor;
+        }
+        public bool Contiene(char Valor)
+        {
+            for (int i = 0; i < tamano; i++) {
+            
+                int indice=(frente+i) % Max;
+                if (Arreglo[indice] == Valor) {
+
+                    return true;
+                 }    
+            }
+            return false;
+        }
+
+
+
+
+    }
+}
---
+++ parcial 2/Codigo/EJERCICIOSDECOLAS/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EJERCICIOSDECOLAS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string resultado = "";
+            Console.WriteLine("COLOQUE SU CADENA DE TEXTO ");
+            string Cadena = Console.ReadLine();
+
+            Cola unaCola = new Cola(Cadena.Length);
+            foreach (char i in Cadena)
+            {
+                if (!unaCola.Contiene(i))
+                {
+                    unaCola.Insertar(i);
+                }
+            }
+
+            while (!unaCola.EstaVacia())
+            {
+                char letra= unaCola.Eliminar();
+                resultado += letra;
+            }
+            Console.WriteLine("La cadena sin duplicados es "+ resultado  );
+            
+
+
+            Console.ReadKey();  
+
+
+        }
+    }
+}
